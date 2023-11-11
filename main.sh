0b571cce024b5371618f0e3c0a0f4c5c7a3c1546
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-11-11 17:45:06 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://gitlab.freedesktop.org/xorg/proto/xorgproto.git -b xorgproto-2023.2
 cp -rvf ./debian ./xorgproto/
 cd ./xorgproto/
+for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
 # Get build deps
 apt-get build-dep ./ -y
